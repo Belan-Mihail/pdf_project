5cbd86837714930a03d1c8366439a82e4102521d
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-09 10:37:38 +0100

    add generate pdf button to SavToPdf component

--- my-pdf-project/src/components/SaveToPdf.tsx
+++ my-pdf-project/src/components/SaveToPdf.tsx
@@ -5,9 +5,13 @@ interface SaveToPdfProp {
     companyName: string
 }
 
-const SaveToPdf = () => {
+const SaveToPdf: React.FC<SaveToPdfProp> = ({companyName}) => {
+
+
   return (
-    <div>SaveToPdf</div>
+    <div>
+        <button type='button'>Generate Pdf</button>
+    </div>
   )
 }
 
