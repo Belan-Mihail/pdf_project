d133cd18bbea5db1027147491bfb0c81854a4a63
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-09 10:55:31 +0100

    edit handleGeneratePdf: add logic to generate pdf

--- my-pdf-project/src/components/SaveToPdf.tsx
+++ my-pdf-project/src/components/SaveToPdf.tsx
@@ -16,6 +16,27 @@ const SaveToPdf: React.FC<SaveToPdfProp> = ({companyName}) => {
         const lineHeight = 8;
         const pageWidth = doc.internal.pageSize.width;
         let yPosition = margin;
+
+        textData.forEach((item) => {
+            doc.setFont(item.isBold ? 'helvetica' : 'helvetica', item.isBold ? 'bold' : 'normal');
+            doc.setFontSize(12);
+      
+            // Break the text into lines so it doesn't go beyond the page
+            const lines = doc.splitTextToSize(item.content.replace("TestName", companyName), pageWidth - 2 * margin);
+      
+            // Add text to the document
+            lines.forEach((line) => {
+              doc.text(line, margin, yPosition);
+              yPosition += lineHeight;
+            });
+      
+            // Add a blank line between paragraphs
+            yPosition += lineHeight;
+          });
+        
+          // Generate PDF
+        doc.save('letter.pdf');
+      
     }
 
   return (
