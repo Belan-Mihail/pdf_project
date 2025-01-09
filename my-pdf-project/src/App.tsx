c3213d6971abf481e7a51ea1d1b9be4407b9cfdd
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-09 10:21:39 +0100

    add handleInputChange to App.tsx

--- my-pdf-project/src/App.tsx
+++ my-pdf-project/src/App.tsx
@@ -5,6 +5,11 @@ import './App.css'
 const App: React.FC = () => {
   const [companyName, setCompanyName] = useState<string>('')
 
+  // handle input change
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCompanyName(e.target.value)
+  }
+
   
   return (
     <div>
