785534c70e8a822061c1092552c45c751fedcf20
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-11 11:19:18 +0100

    define function to save pdf

--- my-pdf-project/src/components/TextEditor.tsx
+++ my-pdf-project/src/components/TextEditor.tsx
@@ -6,7 +6,17 @@ import { jsPDF } from 'jspdf';
 const TextEditor:React.FC = () => {
     const [editorContent, setEditorContent] = useState<string>('')
 
-    
+    // funt to save PDF
+    const saveToPdfFile = () => {
+        const doc = new jsPDF();
+
+        // add text content
+        doc.text(editorContent, 10, 30)
+
+        doc.save('generated_letter.pdf')
+    }
+
+
   return (
     <div>TextEditor</div>
   )
